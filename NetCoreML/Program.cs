f8c99f6270a7145926f112d5be0c6c8ab28961cf
Albert Ahmetzyanov <aaf1974@yandex.ru>
2020-07-03 10:44:08 +0300

    NetCoreML init project

---
+++ NetCoreML/Program.cs
@@ -0,0 +1,34 @@
+﻿using NetCoreLibrary;
+using NetCoreML.SentimentAnalysis;
+using System;
+
+namespace NetCoreML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select one is patterns or END for exite");
+            Tools.OutEnum2Console<MlSampleEnum>();
+            Run();
+
+            SentimentMlSample.Start();
+        }
+
+        public static void Run()
+        {
+            var r = "begin";
+            while (r.ToLower() != "end")
+            {
+                r = Console.ReadLine();
+
+                int intVal;
+                var parse = int.TryParse(r, out intVal);
+                if (parse && Enum.IsDefined(typeof(MlSampleEnum), intVal))
+                    SampleRunner.RunSample((MlSampleEnum)intVal);
+                else
+                    Tools.OutEnum2Console<MlSampleEnum>();
+            }
+        }
+    }
+}
---
+++ NetCoreML/SampleRunner.cs
@@ -0,0 +1,22 @@
+﻿using NetCoreML.SentimentAnalysis;
+using System;
+
+namespace NetCoreML
+{
+    class SampleRunner
+    {
+        internal static void RunSample(MlSampleEnum sample)
+        {
+            switch (sample)
+            {
+                case MlSampleEnum.SentimentAnalysis:
+                    SentimentMlSample.Start();
+                    break;
+
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
