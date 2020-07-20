d218c4f87c47bf477673b7ac5917bd972efddd8e
Albert Ahmetzyanov <aaf1974@yandex.ru>
2020-07-20 17:38:11 +0300

    Add sql examples

---
+++ SqlServerScripts/Samples/SnapshotSample.sql
@@ -0,0 +1,20 @@
+﻿CREATE DATABASE ContainerEditor_s1 ON  
+( NAME = ContainerEditor, FILENAME =   
+'D:\ITrash\SqlSnapshot\ContainerEditor_s1.ss' )  
+AS SNAPSHOT OF ContainerEditor;  
+GO  
+
+
+insert into ContainerEditor..SrvInnerRefs values ('test ref')
+select * from ContainerEditor_s1..SrvInnerRefs
+select * from ContainerEditor..SrvInnerRefs
+
+
+use master
+RESTORE DATABASE ContainerEditor from   
+DATABASE_SNAPSHOT = 'ContainerEditor_s1';  
+
+
+IF EXISTS (SELECT  * FROM sys.databases  
+    WHERE NAME='ContainerEditor_s1')  
+    DROP DATABASE ContainerEditor_s1;  
