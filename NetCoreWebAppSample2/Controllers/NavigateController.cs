ae1d63ccebb8398dbcde7a21751099d05e6b55f4
Albert Ahmetzyanov <aaf1974@yandex.ru>
2020-08-07 10:39:21 +0300

    Add .net Core 3.1 web api sample app

---
+++ NetCoreWebAppSample2/Controllers/NavigateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreWebAppSample2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NavigateController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Навигация";
+        }
+    }
+}
