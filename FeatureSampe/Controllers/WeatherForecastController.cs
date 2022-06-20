170da6a9a2f0816968e9a529a4723c32695010dd
Albert Ahmetzyanov <a.akhmetzyanov@intellectika.ru>
2022-06-20 15:55:25 +0300

    Пример Microsoft.FeatureManagement

---
+++ FeatureSampe/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeatureSampe.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [FeatureGate("Beta")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IFeatureManager _featureManager;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IFeatureManager featureManager, IConfiguration configuration)
+        {
+            _logger = logger;
+            _featureManager = featureManager;
+
+            configuration["FeatureManagement:Beta"] = "true";
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ FeatureSampe/FeatureProviderSamples.cs
@@ -0,0 +1,104 @@
+﻿using FeatureSampe.Controllers;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace FeatureSampe
+{
+    /// <summary>
+    /// <para>https://andrewlock.net/filtering-action-methods-with-feature-flags-adding-feature-flags-to-an-asp-net-core-app-part-2/</para>
+    /// <para>https://stackoverflow.com/questions/50381426/is-there-an-aspnetcore-equivalent-of-the-old-webapi-ihttpcontrollertyperesolver</para>
+    /// <para>https://stackoverflow.com/questions/36680933/discovering-generic-controllers-in-asp-net-core</para>
+    /// <para>https://stackoverflow.com/questions/68084782/how-to-change-configuration-at-runtime-in-asp-net-core</para>
+    /// <para>https://stackoverflow.com/questions/41653688/asp-net-core-appsettings-json-update-in-code</para>
+    /// <para>https://stackoverflow.com/questions/61324768/how-can-i-exclude-a-controller-from-asp-net-core-3-attribute-routing</para>
+    /// <para>https://docs.microsoft.com/ru-ru/azure/azure-app-configuration/use-feature-flags-dotnet-core?tabs=core5x</para>
+    /// </summary>
+    public static class FeatureProviderSamples
+    {
+        public static IServiceCollection Sample(this IServiceCollection services)
+        {
+            return services;
+            /*            services.AddMvc(options => {
+                options.Filters.AddForFeature<FeatureFilterMetadata>("Beta");
+            });*/
+
+            /*            services.AddMvc()
+                                .ConfigureApplicationPartManager(manager =>
+                                {
+                                    var controllerFeatureProvider = manager.FeatureProviders
+                                            .Single(p => p.GetType() == typeof(Microsoft.AspNetCore.Mvc.Controllers.ControllerFeatureProvider));
+                                    manager.FeatureProviders[manager.FeatureProviders.IndexOf(controllerFeatureProvider)] =
+                                       new Attributes.MyControllerFeatureProvider();
+                                });*/
+
+            /*            services
+                            .AddMvc()
+                            .ConfigureApplicationPartManager(apm =>
+                            {
+                                var originals = apm.FeatureProviders.OfType<ControllerFeatureProvider>().ToList();
+                                foreach (var original in originals)
+                                {
+                                    apm.FeatureProviders.Remove(original);
+                                }
+                                apm.FeatureProviders.Add(new SingleControllerFeatureProvider(typeof(AuditSettingController)));
+                            });*/
+
+            /*            services.AddControllers()
+                            .ConfigureApplicationPartManager(manager =>
+                            {
+                                manager.FeatureProviders.Remove(manager.FeatureProviders.OfType<ControllerFeatureProvider>().FirstOrDefault());
+                                manager.FeatureProviders.Add(new CustomControllerFeatureProvider(configuration));
+                            });*/
+        }
+
+
+
+        public class CustomControllerFeatureProvider : ControllerFeatureProvider
+        {
+            private readonly IConfiguration _configuration;
+
+            public CustomControllerFeatureProvider(IConfiguration configuration)
+            {
+                _configuration = configuration;
+            }
+
+            protected override bool IsController(TypeInfo typeInfo)
+            {
+                var isController =
+                    base.IsController(typeInfo)
+                    && typeInfo == typeof(WeatherForecastController)
+                    && true;
+
+                //if (isController && typeInfo.)
+                //{
+                //    var enabledController = _configuration.GetValue<string[]>("EnabledController");
+
+                //    isController = enabledController.Any(x => typeInfo.Name.Equals(x, StringComparison.InvariantCultureIgnoreCase));
+                //}
+
+                return isController;
+            }
+        }
+
+
+        public class SingleControllerFeatureProvider : ControllerFeatureProvider
+        {
+            readonly Type m_type;
+
+            public SingleControllerFeatureProvider(Type type)
+            {
+                m_type = type;
+            }
+
+            protected override bool IsController(TypeInfo typeInfo)
+            {
+                Console.WriteLine(typeInfo.FullName);
+                return false;
+                //return base.IsController(typeInfo) && typeInfo == m_type.GetTypeInfo();
+            }
+        }
+    }
+}
