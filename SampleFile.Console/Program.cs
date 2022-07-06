5776c27a010826c6606346587959b9521762362f
Albert Ahmetzyanov <a.akhmetzyanov@intellectika.ru>
2022-07-07 00:00:24 +0300

    проверка IFormFile

---
+++ SampleFile.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace SampleFile.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Delay(5000);
+            SendFile();
+        }
+
+        public static void SendFile()
+        {
+            using HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:55490/");
+
+            using var form = new MultipartFormDataContent();
+
+            using var fs = File.OpenRead(@"D:\Develop\LerningSolution\LerningSolution\README.md");
+            var streamContent = new StreamContent(fs);
+            var fileContent = new ByteArrayContent(File.ReadAllBytes(@"D:\Develop\LerningSolution\LerningSolution\README.md"));
+
+            fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+
+            // "file" parameter name should be the same as the server side input parameter name
+            form.Add(fileContent, "file", Path.GetFileName(fs.Name));
+            HttpResponseMessage response =  httpClient.PostAsync("WeatherForecast/SaveFile", form).Result;
+        }
+    }
+
+}
---
+++ SampleFile.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SampleFile.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
